00dbd421ed76e863c1d3f13a721bd441c89c171c
Lisa Sousa <lisa@MacBook-Air.fritz.box>
2019-02-27 23:54:28 +0100

    multiple plots on one page for selected features

--- additional_analysis/analysis_enhancer.R
+++ additional_analysis/analysis_enhancer.R
@@ -58,12 +58,15 @@ plot_binary_feature <- function(feature,column_all,column_strongest,column_close
   
   data_set_plot = rbind(column_all,column_strongest,column_close)[,c(4,1,3)]
   colnames(data_set_plot) = c("enhancer_set","overlap","value")
+  data_set_plot$overlap = as.numeric(data_set_plot$overlap)-1
+  data_set_plot$overlap[data_set_plot$overlap==0] = "no"
+  data_set_plot$overlap[data_set_plot$overlap==1] = "yes"
+  data_set_plot$overlap = as.factor(data_set_plot$overlap)
   
-  par(mar=c(15,7,7,7))
   title_box_plot = paste(feature,
-                         "\nwilcox test p-value for all enhancer: ",signif(wilcox_all,3),
-                         "\nwilcox test p-value for strongest enhancer:",signif(wilcox_strongest,3),
-                         "\nwilcox test p-value for closest enhancer:",signif(wilcox_close,3))
+                         "\np-value for all: ",signif(wilcox_all,3),
+                         "\np-value for strongest:",signif(wilcox_strongest,3),
+                         "\np-value for closest:",signif(wilcox_close,3))
   
   ggbox = ggplot(data_set_plot, aes(x = enhancer_set, y = value, fill = overlap)) + geom_boxplot(alpha=0.7,notch = F) +
     scale_y_continuous(name = "half-time[days]") + scale_x_discrete(name = "enhancer set") + ggtitle(title_box_plot) +
@@ -71,10 +74,12 @@ plot_binary_feature <- function(feature,column_all,column_strongest,column_close
                        axis.title = element_text(size=15, face="bold"),
                        axis.text.x = element_text(size=15, angle = 60, hjust = 1),
                        axis.text.y = element_text(size=15),
+                       legend.text=element_text(size=15),
+                       legend.title = element_text(size=15),
                        axis.line = element_line(colour = "black"),
-                       plot.margin = unit(c(2,2,2,2), "cm")) + 
+                       plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) + 
     scale_fill_brewer(palette = "Greys")
-  print(ggbox)
+  return(ggbox)
 }
 
 plot_continuous_feature <- function(feature,column_all,column_strongest,column_close,n){
@@ -92,31 +97,37 @@ plot_continuous_feature <- function(feature,column_all,column_strongest,column_c
   
   data_set_plot = rbind(column_all,column_strongest,column_close)[,c(4,2,1)]
   colnames(data_set_plot) = c("enhancer_set","silencing_class","value")
+  data_set_plot$silencing_class[data_set_plot$silencing_class==0] = "silenced"
+  data_set_plot$silencing_class[data_set_plot$silencing_class==1] = "not silenced"
   data_set_plot$silencing_class = as.factor(data_set_plot$silencing_class)
+  levels(data_set_plot$silencing_class) = factor(data_set_plot$silencing_class, levels = rev(levels(data_set_plot$silencing_class)))
   
-  par(mar=c(15,7,7,7))
   title_box_plot = paste(feature,
-                         "\nwilcox test p-value for all enhancer: ",signif(wilcox_all,3),
-                         "\nwilcox test p-value for strongest enhancer:",signif(wilcox_strongest,3),
-                         "\nwilcox test p-value for closest enhancer:",signif(wilcox_close,3))
+                         "\np-value for all: ",signif(wilcox_all,3),
+                         "\np-value for strongest:",signif(wilcox_strongest,3),
+                         "\np-value for closest:",signif(wilcox_close,3))
   
   ggbox = ggplot(data_set_plot, aes(x = enhancer_set, y = value, fill = silencing_class)) + geom_boxplot(alpha=0.7,notch = F) +
-    scale_y_continuous(name = "half-time[days]") + scale_x_discrete(name = "enhancer set") + ggtitle(title_box_plot) +
+    scale_y_continuous(name = "feature") + scale_x_discrete(name = "enhancer set") + ggtitle(title_box_plot) +
     theme_bw() + theme(plot.title = element_text(size = 16, face = "bold",hjust = 0.5),
                        axis.title = element_text(size=15, face="bold"),
                        axis.text.x = element_text(size=15, angle = 60, hjust = 1),
                        axis.text.y = element_text(size=15),
+                       legend.text = element_text(size=15),
+                       legend.title = element_text(size=15),
                        axis.line = element_line(colour = "black"),
-                       plot.margin = unit(c(2,2,2,2), "cm")) + 
+                       plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) + 
     scale_fill_brewer(palette = "Greys")
-  print(ggbox)
+  return(ggbox)
 }
 
 ###############
 #enhancer sets
 ###############
 
-source("/project/lncrna/Xist/xist_mediated_gene_silencing/modelling/model_functions.R")
+library(ggplot2)
+library(gridExtra)
+
 output_dir = "/Users/lisa/work_stuff/projects/xist_epigenetics/computing/plots/additional_analysis/"
 file_halftimes = paste(dir_data,"silencing_halftimes/fitted_data/halftimes_pro_seq_mm10_RSS_initial_ratio.txt",sep="")
 file_feature_matrix = paste(dir_data,"modelling/feature_matrix/promoter_matrix_normRAdjusted_enhancer.RData",sep="")
@@ -168,21 +179,51 @@ data_set_close = cbind(data_set_close[,10:86],target=data_set_close$target,halft
 
 
 ##plots boxplots
-pdf(paste(output_dir,"enhancer_boxplots.pdf"),height = 10, width = 10)
+pdf(paste(output_dir,"enhancer_boxplots.pdf",sep=""),height = 20, width = 20)
+
+boxplots = list()
 
 for(i in 1:(ncol(data_set_all)-2)){
-  
   feature = colnames(data_set_all)[i]
   column_all = cbind.data.frame(feature=data_set_all[,i],target=data_set_all$target, halftime=data_set_all$halftime, enhancer_set = factor("all"))
   column_strongest = cbind.data.frame(feature=data_set_strong[,i],target=data_set_strong$target, halftime=data_set_strong$halftime, enhancer_set = factor("strongest"))
   column_close = cbind.data.frame(feature=data_set_close[,i],target=data_set_close$target, halftime=data_set_close$halftime, enhancer_set = factor("closest"))
   
-  if(is.factor(column_all$feature)){
-    plot_binary_feature(feature,column_all,column_strongest,column_close,n)
-  }else{
-    plot_continuous_feature(feature,column_all,column_strongest,column_close,n)
+  if(feature %in% hic_inteactions){
+    column_all$feature[column_all$feature > 0] = 1
+    column_all$feature = as.factor(column_all$feature)
     
+    column_strongest$feature[column_strongest$feature > 0] = 1
+    column_strongest$feature = as.factor(column_strongest$feature)
+    
+    column_close$feature[column_close$feature > 0] = 1
+    column_close$feature = as.factor(column_close$feature)
   }
+  if(is.factor(column_all$feature)){
+    boxplots[[i]] = plot_binary_feature(feature,column_all,column_strongest,column_close,n)
+  }else{
+    boxplots[[i]] = plot_continuous_feature(feature,column_all,column_strongest,column_close,n)
+  }
+}
+
+print(boxplots)
+
+dev.off()
+
+##plots boxplots of specific features
+
+pdf(paste(output_dir,"enhancer_boxplots.pdf",sep=""),height = 20, width = 20)
+
+boxplots = list()
+colnumbers = c(5,8,14,28,41,40,43,44,62,64,59,77,60,76,66,68)
+
+for(j in 1:length(colnumbers)){
+  i = colnumbers[j]
+  feature = colnames(data_set_all)[i]
+  column_all = cbind.data.frame(feature=data_set_all[,i],target=data_set_all$target, halftime=data_set_all$halftime, enhancer_set = factor("all"))
+  column_strongest = cbind.data.frame(feature=data_set_strong[,i],target=data_set_strong$target, halftime=data_set_strong$halftime, enhancer_set = factor("strongest"))
+  column_close = cbind.data.frame(feature=data_set_close[,i],target=data_set_close$target, halftime=data_set_close$halftime, enhancer_set = factor("closest"))
+  
   if(feature %in% hic_inteactions){
     column_all$feature[column_all$feature > 0] = 1
     column_all$feature = as.factor(column_all$feature)
@@ -193,8 +234,15 @@ for(i in 1:(ncol(data_set_all)-2)){
     column_close$feature[column_close$feature > 0] = 1
     column_close$feature = as.factor(column_close$feature)
     
-    plot_binary_feature(feature,column_all,column_strongest,column_close,n)
+    #plot_binary_feature(feature,column_all,column_strongest,column_close,n)
+  }
+  if(is.factor(column_all$feature)){
+    boxplots[[j]] = plot_binary_feature(feature,column_all,column_strongest,column_close,n)
+  }else{
+    boxplots[[j]] = plot_continuous_feature(feature,column_all,column_strongest,column_close,n)
   }
 }
+
+grid.arrange(grobs=boxplots, nrow = 4)
 
 dev.off()

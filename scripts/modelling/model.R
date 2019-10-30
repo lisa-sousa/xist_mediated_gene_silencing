fd42c28baebabb2718fa731c2f7b0ca4b19824f1
LiBa Sousa e Thieme <lisa.barros.andrade.sousa@gmail.com>
2019-10-30 11:44:07 +0100

    changes number of runs

--- scripts/modelling/model.R
+++ scripts/modelling/model.R
@@ -61,7 +61,7 @@ ncores = 20 # number of cores for parallelization
 test_set_min_size = 10 # minimum number of genes in OOB set
 ntree = 1000 # number of trees in the random forest 
 thr_class_error = 0.05 # maximal difference in error between class 0 and class 1, e.g. if class 0 has error 0.2 then class 1 has to have an error between 0.15 and 0.25
-runs = 100 # number of runs for stability test of random forest model
+runs = 500 # number of runs for stability test of random forest model
 B=300 #number of bootstraps for cluster stability analysis
 thr_anova_p_value = 0.05 # p-value threshold for anova test
 idx_RPKM = c(10,15) #idx_RPKM = c(5) #idx_RPKM = c(5,7)
